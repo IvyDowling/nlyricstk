48810cfc3be7463d01fc459f6308fd6468d26533
iv <iv@ivDesktop>
2017-03-19 02:08:27 -0400

    lazy-grams, but the result is funny

--- nlyricstk.py
+++ nlyricstk.py
@@ -9,42 +9,30 @@ def run(fn, *inputfile):
     for f in inputfile:
         this_file = open(f)
         for ln in this_file:
-            # \n
-            # ln = ln.strip()
-            # ln += " 'END'"
-            if ln is not "":
-                # splits on words
-                words = re.split('([\w\-\']+)', ln)
-                # delete spaces and empty strings
-                words = [w for w in words
+            # there are a bunch of credit headers above the songs
+            if "[" not in ln:
+                # if you leave these in, it naturally breaks lines
+                # ln = ln.strip()
+                if ln is not "":
+                    # splits on words
+                    words = re.split('([\w\-\']+)', ln)
+                    # delete spaces and empty strings
+                    [huge_list.append(w) for w in words
                          if (w is not " " and
                              w is not "" and
                              w is not "(" and
                              w is not ")" and
-                             w is not "[" and
-                             w is not "]" and
                              w is not '"')]
-                for w in words:
-                    huge_list.append(w)
 
     print("Got input file, now generating a song...")
     # 4-gram
-    gram_size = 4
+    gram_size = 3
     ngram = list(zip(*[huge_list[i:] for i in range(gram_size)]))
     ngram = [list(i) for i in ngram]
     # generate song
-    #
     song = ""
     phrase = ngram[rand.randint(0, len(ngram))]
     while True:
-        """
-        if "'END'" in phrase:
-            phrase = phrase[0: phrase.index("'END'")]
-            phrase.append("\n")
-            for p in phrase:
-                song += " " + str(p)
-            phrase = ngram[rand.randint(0, len(ngram))]
-        """
         swap = phrase.copy()
         for g in ngram:
             # print(str(g[:-1]) + " " + str(phrase[2:]))
