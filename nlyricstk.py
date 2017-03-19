2561b8765a42663afb0658f9d19b17a2bdeea8fd
iv <iv@ivDesktop>
2017-03-19 01:59:09 -0400

    well, i guess this is good

--- nlyricstk.py
+++ nlyricstk.py
@@ -10,8 +10,8 @@ def run(fn, *inputfile):
         this_file = open(f)
         for ln in this_file:
             # \n
-            ln = ln.strip()
-            ln += " 'END'"
+            # ln = ln.strip()
+            # ln += " 'END'"
             if ln is not "":
                 # splits on words
                 words = re.split('([\w\-\']+)', ln)
@@ -19,13 +19,17 @@ def run(fn, *inputfile):
                 words = [w for w in words
                          if (w is not " " and
                              w is not "" and
+                             w is not "(" and
+                             w is not ")" and
+                             w is not "[" and
+                             w is not "]" and
                              w is not '"')]
                 for w in words:
                     huge_list.append(w)
 
     print("Got input file, now generating a song...")
     # 4-gram
-    gram_size = 3
+    gram_size = 4
     ngram = list(zip(*[huge_list[i:] for i in range(gram_size)]))
     ngram = [list(i) for i in ngram]
     # generate song
@@ -33,20 +37,33 @@ def run(fn, *inputfile):
     song = ""
     phrase = ngram[rand.randint(0, len(ngram))]
     while True:
+        """
         if "'END'" in phrase:
             phrase = phrase[0: phrase.index("'END'")]
             phrase.append("\n")
             for p in phrase:
                 song += " " + str(p)
             phrase = ngram[rand.randint(0, len(ngram))]
+        """
         swap = phrase.copy()
         for g in ngram:
-            if g[:-1] == phrase[1:]:
-                phrase.append(g[-1])
+            # print(str(g[:-1]) + " " + str(phrase[2:]))
+            if g[0] == phrase[-1]:
+                phrase.extend(g[1:])
                 window = phrase[-(gram_size - 1):]
+                rand.shuffle(ngram)
+                break
         if swap == phrase:
+            for p in phrase:
+                song += " " + str(p)
+            song += "\n"
             phrase = ngram[rand.randint(0, len(ngram))]
-        if len(song) > 500:
+
+
+        if len(phrase) > 200:
+            for p in phrase:
+                song += " " + str(p)
+            song += "\n"
             break
     print(song)
     # tts
